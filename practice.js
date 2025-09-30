52de9a63a1a361beeaa117eaeceeb4af954cd2b4
MosesKollam <StephenMosesKollam@outlook.com>
2025-09-30 11:15:12 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+
+const basepath = `${__dirname}`;
+const index = fs.readFileSync(path.join(basepath, 'practice.html'), 'utf-8');
+
+const server = http.createServer(function (request, response) {
+    const parsedUrl = url.parse(request.url, true);
+    const pathname = parsedUrl.pathname;
+
+    if (pathname === "/" || pathname === "/overview") {
+        response.writeHead(200, { 'Content-type': 'text/html' });
+        response.end(index);
+    } else {
+        let contentType = 'text/plain';
+        const forward_path = path.join(basepath, pathname);
+        fs.readFile(forward_path, function (error, data) {
+            const ext = path.extname(forward_path);
+            if (ext === '.css') contentType = "text/css";
+            if (ext === '.js') contentType = "application/javascript";
+            if (ext === '.png') contentType = "image/png";
+            if (ext === '.jpg') contentType = "image/jpg";
+            if (ext === '.jpeg') contentType = "image/jpeg";
+            if (ext === '.html') contentType = "text/html";
+            response.writeHead(200, { 'Content-type': contentType });
+            response.end(data);
+        })
+    }
+});
+
+server.listen(8000, '127.0.0.1', function () {
+    console.log('Server running at http://127.0.0.1:8000');
+});
